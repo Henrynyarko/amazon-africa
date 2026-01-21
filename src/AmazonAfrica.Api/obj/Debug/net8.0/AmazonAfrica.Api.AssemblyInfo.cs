99c33620cf88b41ab67e1b6b9a7a66ae20233251
user <user@Users-MacBook-Pro-3.local>
2026-01-21 09:02:21 +0000

    Resolving error

--- src/AmazonAfrica.Api/obj/Debug/net8.0/AmazonAfrica.Api.AssemblyInfo.cs
+++ src/AmazonAfrica.Api/obj/Debug/net8.0/AmazonAfrica.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AmazonAfrica.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4e5cc924e745ea8b3c48083ab937fff424b403d6")]
 [assembly: System.Reflection.AssemblyProductAttribute("AmazonAfrica.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AmazonAfrica.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
