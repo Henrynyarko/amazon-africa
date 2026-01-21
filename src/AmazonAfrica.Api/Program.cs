ae3c34a79e9a98881a6cee350d118dcc306e3a46
user <user@Users-MacBook-Pro-3.local>
2026-01-21 11:15:43 +0000

    Add EF Core DbContext and entities

--- src/AmazonAfrica.Api/Program.cs
+++ src/AmazonAfrica.Api/Program.cs
@@ -1,19 +1,26 @@
+using Microsoft.EntityFrameworkCore;
+using AmazonAfrica.Api.Data;
+using AmazonAfrica.Api.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services for controllers
+// 1. Add PostgreSQL DB context
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseNpgsql(connectionString));
+
+// 2. Add controllers and Swagger
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Enable Swagger everywhere
+// 3. Enable Swagger everywhere
 app.UseSwagger();
 app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
-
-// Map controllers
 app.MapControllers();
 
 app.Run();
